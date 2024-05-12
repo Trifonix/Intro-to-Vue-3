8d5f2359d705039f36f368552628b5cda2629205
Trifonix <trifonix@rocketmail.com>
2024-05-12 13:39:19 +0300

    add image and use v-bind directive

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const app = Vue.createApp({
     return {
       product: "Носки",
       description: "Ноги скажут 'спасибо'",
+      image: "./assets/images/socks_green.jpg",
+      url: "https://ru.wikipedia.org/wiki/%D0%9D%D0%BE%D1%81%D0%BA%D0%B8",
     };
   },
 });
