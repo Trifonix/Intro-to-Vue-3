8a5a88cb07d6e77832ee23cfd5944b55fe1a9f39
Trifonix <trifonix@rocketmail.com>
2024-05-12 17:58:55 +0300

    add lists of details and sizes

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ const app = Vue.createApp({
       url: "https://ru.wikipedia.org/wiki/%D0%9D%D0%BE%D1%81%D0%BA%D0%B8",
       inventory: 20,
       onSale: true,
+      details: ["Хлопок 50%", "Шерсть 30%", "Полиэстер 20%"],
+      sizes: ["S", "M", "L", "XL"],
     };
   },
 });
