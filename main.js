eb723e4cc3b956ff18a54074ec05a3824ce673ad
Trifonix <trifonix@rocketmail.com>
2024-05-12 13:08:46 +0300

    mount app with two data items

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-const product = 'Socks'
+const app = Vue.createApp({
+  data() {
+    return {
+      product: "Носки",
+      description: "Ноги скажут 'спасибо'",
+    };
+  },
+});
+const mountedApp = app.mount("#app");
