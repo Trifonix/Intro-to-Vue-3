c998f4c1f2995ecf6a3c6a99809282b0f7731c1b
Trifonix <trifonix@rocketmail.com>
2024-05-12 19:06:14 +0300

    and variants data and two buttons with cart div

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const app = Vue.createApp({
   data() {
     return {
+      cart: 0,
       product: "Носки",
       description: "Ноги скажут 'спасибо'",
       image: "./assets/images/socks_green.jpg",
@@ -9,7 +10,22 @@ const app = Vue.createApp({
       onSale: true,
       details: ["Хлопок 50%", "Шерсть 30%", "Полиэстер 20%"],
       sizes: ["S", "M", "L", "XL"],
+      variants: [
+        { id: 901, color: "Зеленые", image: "./assets/images/socks_green.jpg" },
+        { id: 902, color: "Синие", image: "./assets/images/socks_blue.jpg" },
+      ],
     };
+  },
+  methods: {
+    addToCart() {
+      this.cart++;
+    },
+    updateImage(variantImage) {
+      this.image = variantImage;
+    },
+    removeFromCart() {
+      if (this.cart > 0) this.cart--;
+    },
   },
 });
 const mountedApp = app.mount("#app");
